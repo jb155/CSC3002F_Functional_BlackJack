dd1a51068b05d86f4340ea6db38f7b497e4c5697
BTHJAC013 <bthjac013@muct.ac.za>
2016-05-12 12:26:11 +0200

    Q1 done

--- BTHJAC013-twenty-one.scm
+++ BTHJAC013-twenty-one.scm
@@ -15,14 +15,32 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;  Question 1.  Code for "best-hand"
+;; Check if current hand has the nearest value to 21 as possible, if it has an Ace in it hand, see if 
+;; bieng a one or eleven produces a more desireable result.
+;; Determine whether the hand thus far should consider its aces as ones or elevens
+;; Test-cases: 
+;;	- input: '((7 s) (A d) (3 h)) output: 21
+;;	- input: '((7 s) (A d) (3 h) (5 s)) output: 16
+;;	- input: '((3 c) (A d) (3 h) (A s)) output: 19
+;;	- input: '((A c) (A d) (A h) (A s)) output: 14
+;;
+;; Algorithm:
+;;	- there can be at most four aces in a hand, limited by the number of suits
+;;	- there can be at most one ace worth eleven points, as having two aces
+;;	  both worth eleven points would result in the player losing the game
+;;	- the difference between the worth of a hand when an ace is worth one and 
+;;	  when an ace is worth eleven is ten points
+;;	- therefore initially calculate the total score when the ace is worth one
+;;	  and see if by adding ten (i.e., making one of the aces worth eleven 
+;;	  instead) the score is not yet greater than 21
+;;	- if the result is in favour of eleven, return that result, otherwise
+;;	  return the initial result
 
-;; Dummy Version of best-hand
 (define (best-hand hand)
-   (min-val hand))
+   (if (<= (aces-eleven hand) 21) (aces-eleven hand) (min-val hand)))
 
-;; Best Value of Hand
-;; [add description and test -cases]
-
+(define (aces-eleven hand)
+   (+ (min-val hand) 10))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;  Question 2.  "stop-at"

65aa766d5b2b6427bb5502f2865f5bd49c971b4d
BTHJAC013 <bthjac013@muct.ac.za>
2016-05-12 09:04:56 +0200

    Base project files

---
+++ my-twenty-one.scm
@@ -0,0 +1,68 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;      CS 3 Scheme programming assignment               ;;;;
+;;;;                 April 2016                            ;;;;
+;;;;        <joe blogg's> solutions <stdnum000>            ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  
+;; This depends on "cs3-black-jack.scm"
+;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;  Your code goes here                                    ;;;;
+;;;;  Submit a file of code of everything you created below  ;;;;
+;;;;  please do not submit the predefined code              ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;  Question 1.  Code for "best-hand"
+
+;; Dummy Version of best-hand
+(define (best-hand hand)
+   (min-val hand))
+
+;; Best Value of Hand
+;; [add description and test -cases]
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;  Question 2.  "stop-at"
+;; [add description and test -cases]
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;  Question 3.  "repeat-game"
+;; [add description and test -cases]
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Question 4.    clever
+;; [add description and test -cases]
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;      Question 5.                Majority
+;;;
+;; [add description and test -cases]
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;        Question 6.              Get Stats
+;;;
+;; [add description and test -cases]
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;   Question 8.   interactive 
+;; [add description and user insctructions]
+
+; function to get the input returns #t if the user types y otherwise #f
+(define (hit-me?)
+  (eq? (read) 'y))
+
